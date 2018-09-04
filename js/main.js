e7f6e6d33b790c35d3cf2683ab5b48044c587cff
mt-taro <tm.pos97@gmail.com>
2018-09-05 00:10:38 +0900

    ドラえもん秘密道具占い作成

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function(){
+  'use strict';
+
+  var btn = document.getElementById('btn');
+
+  btn.addEventListener('click', function(){
+    var n = Math.random();
+    if(n < 0.05){ //5%
+      this.textContent = 'チンポ (ﾎﾞﾛﾝ';
+    } else {  //95%
+      var tools = ['タイムマシーン～', 'タケコプタ～', 'どこでもドア～', '通り抜けフープ～', 'バイバイン～',
+        'デンデンハウス～', 'さいなん報知器～', '代用シール～', 'もしもボックス～', 'ひらりマント～', 'フエルミラ～',
+        'ガリバートンネル～', 'あべこべクリーム～', 'タイムカメラ～', '爆発コショウ～', '時限バカ弾～', 'コエカタマリン～'];
+      var m = Math.floor(Math.random() * tools.length);
+      this.textContent = tools[m];
+    }
+  });
+  btn.addEventListener('mousedown', function(){
+    this.className = 'pushed';
+  });
+  btn.addEventListener('mouseup', function(){
+    this.className = '';
+  });
+})();
